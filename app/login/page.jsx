45a9dba2be562eb5f9f372c104580a059ad12224
Buyaki01 <rittahbuyaki@gmail.com>
2023-10-31 16:26:47 +0300

    Add functionality where if user is logged in they should not access the register and login pages

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,14 @@
+import { getServerSession } from "next-auth"
 import FormWrap from "../components/FormWrap"
 import LoginForm from "./LoginForm"
+import { authOptions } from "../api/auth/[...nextauth]/route"
+import { redirect } from "next/navigation"
 
-const Login = () => {
+const Login = async () => {
+  const session = await getServerSession(authOptions)
+
+  if (session) redirect("/")
+
   return (
     <FormWrap>
       <LoginForm />
--- app/register/page.jsx
+++ app/register/page.jsx
@@ -1,7 +1,14 @@
+import { getServerSession } from "next-auth"
 import FormWrap from "../components/FormWrap"
 import RegisterForm from "./RegisterForm"
+import { authOptions } from "../api/auth/[...nextauth]/route"
+import { redirect } from "next/navigation"
 
-const Register = () => {
+const Register = async () => {
+  const session = await getServerSession(authOptions)
+
+  if (session) redirect("/")
+  
   return (
     <FormWrap>
       <RegisterForm />
