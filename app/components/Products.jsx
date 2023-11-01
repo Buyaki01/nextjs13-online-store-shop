dadbf917a6701834355e1c693ed48a770cf8d373
Buyaki01 <rittahbuyaki@gmail.com>
2023-11-01 09:04:45 +0300

    Add loading message in Products component

--- app/components/Products.jsx
+++ app/components/Products.jsx
@@ -6,11 +6,13 @@ import { useEffect, useState } from "react"
 
 const Products = () => {
   const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
   
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await axios('/api/products')
       setProducts(response.data)
+      setLoading(false)
     }
 
     fetchProducts()
@@ -18,26 +20,32 @@ const Products = () => {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 items-center justify-center">
-      {products.length > 0 && products.map((product) => (
-        <div key={product._id} className="border p-4 rounded-lg text-center">
-          <div>
-            <Link href={`/products/${product._id}`}>
-              <img 
-                src={product.uploadedImagePaths[0]} 
-                alt={product.productName}
-                className="w-40 h-40 object-cover cursor-pointer mx-auto"
-              />
-            </Link>
-          </div>
-
-          <div className="mt-4">
-            <Link href={`/products/${product._id}`}>
-              <p className="text-lg font-semibold hover:underline cursor-pointer">{product.productName}</p>
-            </Link>
-            <h4 className="text-xl font-bold mt-2 text-center">Ksh. {product.price}</h4>
-          </div>
-        </div>
-      ))}
+      {loading 
+        ? (
+            <p className="loadingMessage">Loading...</p>
+          )
+        : (products.length > 0 && products.map((product) => (
+            <div key={product._id} className="border p-4 rounded-lg text-center">
+              <div>
+                <Link href={`/products/${product._id}`}>
+                  <img 
+                    src={product.uploadedImagePaths[0]} 
+                    alt={product.productName}
+                    className="w-40 h-40 object-cover cursor-pointer mx-auto"
+                  />
+                </Link>
+              </div>
+    
+              <div className="mt-4">
+                <Link href={`/products/${product._id}`}>
+                  <p className="text-lg font-semibold hover:underline cursor-pointer">{product.productName}</p>
+                </Link>
+                <h4 className="text-xl font-bold mt-2 text-center">Ksh. {product.price}</h4>
+              </div>
+            </div>
+          ))
+        )
+      }
     </div>
   )
 }
