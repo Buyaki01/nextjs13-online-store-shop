aa6dfab87a7477d61e54b8344307c5b27c08d4c0
Buyaki01 <rittahbuyaki@gmail.com>
2023-11-02 19:20:05 +0300

    Change button for Start Shopping to Link

--- app/cart/page.jsx
+++ app/cart/page.jsx
@@ -147,7 +147,14 @@ const Cart = () => {
                 <div className="flex flex-col items-center justify-center">
                   <p className="mt-5 text-2xl">Your cart is empty!</p>
                   <div className="mt-5">
-                    <Link href={'/'}><button className="text-white text-lg px-4 py-2 rounded-lg focus:outline-none">Start Shopping</button></Link>
+                    <Link 
+                      href={'/'}
+                      className="text-xl font-bold text-secondary flex gap-1 items-center justify-center hover:underline"
+                    >
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                      </svg>Start Shopping
+                    </Link> 
                   </div>
                 </div>
               )
