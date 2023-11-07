377d9d58b85645e54226e3146025b260a5e7b076
Buyaki01 <rittahbuyaki@gmail.com>
2023-11-07 12:07:14 +0300

    Install react-slick and slick-carousel. Style Flash Sales component with react-slick and slick-carousel

--- app/components/FlashSales.jsx
+++ app/components/FlashSales.jsx
@@ -1,142 +1,68 @@
-import Link from "next/link";
-import styled from "styled-components"
-
-const FlashSalesGrid = styled.article`
-  display: grid;
-  grid-template-columns: 2fr 3fr 3fr 3fr 2fr;
-  grid-template-rows: 50px 450px;
-  grid-column-gap: 10px;
-  background-color: rgb(146, 212, 59);
-  margin-bottom: 50px;
-  padding-bottom: 25px;
-
-  h4{
-    color: #d40d9a;
-  }
-
-  img{
-    max-width: 100%;
-    max-height: 300px;
-  }
-`;
-
-const FlashSalesTitleGrid = styled.div`
-  grid-row: 1;
-  grid-column: 2/ span 3;
-  display: flex;
-  align-items: center;
-
-  h3{
-    font-size: 1.5rem;
-  }
-`;
-
-const FlashSalesProductsGridOne = styled.div`
-  grid-row: 2;
-  grid-column: 2;
-  position: relative;
-  display: flex;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-  flex-direction: column;
-  min-width: 0;
-  word-wrap: break-word;
-  background-color: #fff;
-  background-clip: border-box;
-  border: 1px solid rgba(0,0,0,.125);
-  border-radius: 0.25rem;
-`;
-
-const FlashSalesProductsGridTwo = styled.div`
-  grid-row: 2;
-  grid-column: 3;
-  position: relative;
-  display: flex;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-  flex-direction: column;
-  min-width: 0;
-  word-wrap: break-word;
-  background-color: #fff;
-  background-clip: border-box;
-  border: 1px solid rgba(0,0,0,.125);
-  border-radius: 0.25rem;
-`;
-
-const FlashSalesProductsGridThree = styled.div`
-  grid-row: 2;
-  grid-column: 4;
-  position: relative;
-  display: flex;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  -webkit-box-orient: vertical;
-  -webkit-box-direction: normal;
-  flex-direction: column;
-  min-width: 0;
-  word-wrap: break-word;
-  background-color: #fff;
-  background-clip: border-box;
-  border: 1px solid rgba(0,0,0,.125);
-  border-radius: 0.25rem;
-`;
-
-const FlashSalesLinks = styled(Link)`
-  text-decoration: none;
-  white-space: nowrap;
-  color: #212529;
-`
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 const FlashSales = () => {
+  const [flashSalesProducts, setFlashSalesProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchFlashSalesProducts = async () => {
+      const response = await axios('/api/products')
+      setFlashSalesProducts(response.data)
+      setLoading(false)
+    }
+
+    fetchFlashSalesProducts()
+  }, [])
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  }
+
   return (
-    <FlashSalesGrid>
-      <FlashSalesTitleGrid>
-        <h3>Flash Sales</h3>
-      </FlashSalesTitleGrid>
+    <article className="mb-12">
+      <div className="text-center mb-1">
+        <h2 className="text-3xl font-semibold">Flash Sales</h2>
+      </div>
 
-      <FlashSalesProductsGridOne>
-        <div>
-          <FlashSalesLinks href={'/flashsales/id'}>
-            <img src="http://localhost:3000/images/ToteBag.png" alt="" />
-          </FlashSalesLinks>
-        </div>
-        <div>
-          <FlashSalesLinks href={'/flashsales/id'}><p>Product Name</p></FlashSalesLinks>
-          <h4>Price</h4>
-        </div>
-      </FlashSalesProductsGridOne>
-
-      <FlashSalesProductsGridTwo>
-        <div>
-          <FlashSalesLinks href={'/flashsales/id'}>
-            <img src="http://localhost:3000/images/YellowBag.png" alt="" />
-          </FlashSalesLinks>
-        </div>
-        <div>
-          <FlashSalesLinks href={'/flashsales/id'}><p>Product Name</p></FlashSalesLinks>
-          <h4>Price</h4>
-        </div>
-      </FlashSalesProductsGridTwo>
-
-      <FlashSalesProductsGridThree>
-        <div>
-          <FlashSalesLinks href={'/flashsales/id'}>
-            <img src="http://localhost:3000/images/LeatherDesignerBag.png" alt="" />
-          </FlashSalesLinks>
-        </div>
-        <div>
-          <FlashSalesLinks href={'/flashsales/id'}><p>Product Name</p></FlashSalesLinks>
-          <h4>Price</h4>
-        </div>
-      </FlashSalesProductsGridThree>
-    </FlashSalesGrid>
+      <div className="flashSalesContainer px-12 py-5">
+        {loading 
+          ? (
+              <p className="loadingMessage">Loading...</p>
+            )
+          :(
+            <Slider {...settings} className="my-8 py-4">
+              {flashSalesProducts.length > 0 && flashSalesProducts.map((product) => (
+                <div className="flex flex-col justify-center items-center h-[350px] rounded-lg border border-gray-200">
+                  <div className="p-2">
+                    <Link href={`/products/${product._id}`}>
+                      <img 
+                        src={product.uploadedImagePaths[0]} 
+                        alt={product.productName}
+                        className="w-48 h-48 object-cover cursor-pointer mx-auto p-2"
+                      />
+                    </Link>
+                  </div>
+                  <div className="text-center mt-4">
+                    <Link href={`/products/${product._id}`}>
+                      <p className="text-xl font-semibold hover:underline">{product.productName}</p>
+                    </Link>
+                    <h4 className="text-lg font-semibold">Ksh. {product.price}</h4>
+                  </div>
+                </div>
+              ))}
+            </Slider>
+          )}
+      </div>
+    </article>
   )
 }
 
