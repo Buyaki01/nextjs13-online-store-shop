287db2de513939dbabd6a447b1b773367ef786d3
Buyaki01 <rittahbuyaki@gmail.com>
2023-11-08 14:18:22 +0300

    Update api/webhook

--- app/api/stripe-webhook/route.js
+++ app/api/stripe-webhook/route.js
@@ -3,12 +3,13 @@ import { NextResponse } from 'next/server'
 import connectMongoDB from '@/lib/mongoose'
 import { buffer } from 'micro'
 
-const endpointSecret = "whsec_981dd829484907ce6f8cc5cf31f1b2fc55922abd0f76a4dd6f28121cc341321f"
+const endpointSecret = process.env.ENDPOINT_SECRET
 
 export const POST = async (request, response) => {
   await connectMongoDB()
 
   const sig = request.headers['stripe-signature']
+  console.log(sig)
 
   let event
 
@@ -16,16 +17,15 @@ export const POST = async (request, response) => {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
     event = stripe.webhooks.constructEvent(await buffer(req), sig, endpointSecret)
   } catch (err) {
-    response.status(400).send(`Webhook Error: ${err.message}`)
-    return
+    return NextResponse.json({ error: err.message})
   }
 
   switch (event.type) {
-    case 'checkout.session.completed':
-      const checkoutSessionCompleted = event.data.object
-      console.log(checkoutSessionCompleted)
+    case 'payment_intent.succeeded':
+      const paymentIntentSucceeded = event.data.object
+      console.log(paymentIntentSucceeded)
       break;
     default:
-      console.log(`Unhandled event type ${event.type}`);
+      console.log(`Unhandled event type ${event.type}`)
   }
 }
