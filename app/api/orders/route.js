9f052792ba96852b6a3b09612e296944c2590284
Buyaki01 <rittahbuyaki@gmail.com>
2023-11-10 09:20:09 +0300

    Add Orders table and GET orders route

---
+++ app/api/orders/route.js
@@ -0,0 +1,15 @@
+import connectMongoDB from "@/lib/mongoose"
+import { Order } from "@/models/order"
+import { NextResponse } from "next/server"
+
+export async function GET() {
+  try {
+    await connectMongoDB()
+
+    const orders = await Order.find()
+
+    return NextResponse.json(orders)
+  } catch (error) {
+    return NextResponse.json({ error: "Failed to fetch orders" }, 500)
+  }
+}
--- app/my-orders/page.jsx
+++ app/my-orders/page.jsx
@@ -4,7 +4,27 @@ const MyOrders = () => {
   return (
     <>
       <Header />
-      <div>My Orders</div>
+      <div className="mt-3">
+        <h2 className="uppercase mb-3">Orders</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>DATE</th>
+              <th>TOTAL</th>
+              <th>PAYMENT STATUS</th>
+              <th>DELIVERY STATUS</th>
+              <th>ACTION</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
