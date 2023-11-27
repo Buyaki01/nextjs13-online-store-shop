508fcb59539d230343a3458aa99f7b351b96cc24
Buyaki01 <rittahbuyaki@gmail.com>
2023-11-27 19:22:03 +0300

    Add a slider for the featured product section

--- app/components/Featured.jsx
+++ app/components/Featured.jsx
@@ -1,6 +1,8 @@
 'use client'
 
-import styled from "styled-components"
+import Slider from "react-slick"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
 import { useEffect, useState } from "react"
 import axios from "axios"
 import Link from "next/link"
@@ -19,17 +21,45 @@ const Featured = () => {
     fetchFeaturedProduct()
   }, [])
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  }
+
   return (
-    <div>
+    <div className="bg-secondary pt-5 px-12">
       {loading 
-        ? ( <p>Loading...</p> ) 
+        ? ( <p className="loadingMessage text-white">Loading...</p> ) 
         : products.length > 0 
           ? (
-              <>
+            <div>
+              <Slider {...settings}>
                 {products.map(product => (
-                  <img src={product.uploadedImagePaths[0]}  alt={product.productName} />
+                  <div className="h-[300px]">
+                    <Link 
+                      href={`/products/${product._id}`}
+                      className="flex justify-center"
+                    > {/* User should be directed to the categories page */}
+                      <div className="flex gap-2"> 
+                        <div className="p-2 flex items-center justify-center">
+                          <h2 className="text-white font-bold text-2xl">{product.productName}</h2>
+                        </div>
+                        <div>
+                          <img 
+                            src={product.uploadedImagePaths[0]}  
+                            alt={product.productName} 
+                            className="w-[250px] h-[250px]"
+                          />
+                        </div>
+                      </div>
+                    </Link>
+                  </div>
                 ))}
-              </>
+              </Slider>
+            </div>
             ) 
           : ( <p>No featured product available.</p> )
       }
