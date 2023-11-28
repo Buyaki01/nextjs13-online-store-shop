49280a9c55d7a88a56ea5a76d182746cea62dc3b
Buyaki01 <rittahbuyaki@gmail.com>
2023-11-28 14:32:43 +0300

    Update no payment section in checkout-success page

--- app/checkout-success/[id]/page.jsx
+++ app/checkout-success/[id]/page.jsx
@@ -76,33 +76,32 @@ const CheckoutSuccess = () => {
                 </>
               )
               : (
-                <>
-                  <h3 className="text-4xl my-2">No payment done yet...</h3>
-                  <div className="mt-3 flex gap-2">
-                    <div>
+                <div>
+                  <h3 className="text-4xl font-bold my-2">No payment done yet...</h3>
+                  <p className="text-base my-2">Please click the button below to complete your payment or continue with shopping</p>
+                  <div className="flex gap-2">
+                    <button className="bg-primary text-white text-lg py-2 px-4 rounded-md focus:outline-none">
                       <Link 
-                          href={"/"}
-                          className="flex gap-2 items-center justify-center bg-secondary text-white text-lg px-4 py-2 rounded-md"
-                        >
-                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
-                          </svg>
-                          Continue Shopping
-                        </Link>
-                    </div>
-                    <div>
-                      <Link 
-                        href={"/orders"}
-                        className="flex gap-2 items-center justify-center bg-[#d40d9a] text-white text-lg px-4 py-2 rounded-md"
-                      > 
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M3 3h2c1.591 0 3 .549 4.012 1.461M3 7h2c.97 0 1.915.422 2.568 1.182M3 11h2m9-8v2m0 16v-2m-4-4v2m0 8v-2m1-5.719V21M19 16l3 3m0-6l-3 3m-2.414-3L14 9.414 15.414 8z" />
-                        </svg>
-                        View Orders
+                        href={'/checkout-address'}
+                        className="flex gap-2 items-center justify-center"
+                      >
+                        Checkout
                       </Link>
-                    </div>
+                    </button>
+
+                    <button className="bg-secondary text-white text-lg py-2 px-4 rounded-md focus:outline-none">
+                      <Link 
+                        href={"/"}
+                        className="flex gap-2 items-center justify-center"
+                      >
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                          <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+                        </svg>
+                        Continue Shopping
+                      </Link>
+                    </button>
                   </div>
-                </> 
+                </div> 
               )
             )    
         }
