21a6a444dd362b3f6653a33280a240bda636c1e4
Buyaki01 <rittahbuyaki@gmail.com>
2023-12-06 09:14:27 +0300

    Add Footer component and add categoryImage to Category model

--- models/category.js
+++ models/category.js
@@ -1,8 +1,9 @@
-import mongoose, { Schema, model, models } from "mongoose"
+import { Schema, model, models } from "mongoose"
 
 const CategorySchema = new Schema({
   name: { type: String, required: true },
-  properties: [{ type: Object }]
+  properties: [{ type: Object }],
+  categoryImage: [{ type: String }]
 })
 
 const Category = models?.Category || model("Category", CategorySchema)
