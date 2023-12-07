931d5a0415b50d44141e4e6c086372c638692b99
Buyaki01 <rittahbuyaki@gmail.com>
2023-12-07 10:40:40 +0300

    Install validator, Add GET route for seaching a product or category

---
+++ app/api/products/search/route.js
@@ -0,0 +1,31 @@
+import connectMongoDB from "@/lib/mongoose"
+import Product from "@/models/product"
+import { NextResponse } from "next/server"
+import validator from "validator"
+
+export async function GET(request) {
+  try {
+    await connectMongoDB()
+
+    const products = await Product.find().populate('selectedCategory').populate('brand')
+    const { searchParams } = new URL(request.url)
+    const query = searchParams.get('query')
+
+    // Sanitize and validate the query parameter
+    query = validator.escape(query)
+
+    const filteredSearchProducts = products.filter((product) => {
+      return (
+        product.productName.toLowerCase().includes(query.toLowerCase()) || 
+        product.selectedCategory.name.toLowerCase().includes(query.toLowerCase())
+      )
+    })
+
+    console.log("This is the filteredSearchProducts: ", filteredSearchProducts)
+
+    return NextResponse.json({ filteredSearchProducts })
+  } catch (error) {
+    console.error("Error in search:", error)
+    return NextResponse.error()
+  }
+}
