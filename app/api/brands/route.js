86cbca853c4ebf93c67aadd0518046972b574722
Buyaki01 <rittahbuyaki@gmail.com>
2023-12-13 07:31:10 +0300

    Update product-details and brands api route

--- app/api/brands/route.js
+++ app/api/brands/route.js
@@ -1,10 +1,11 @@
 import connectMongoDB from "@/lib/mongoose"
 import Brand from "@/models/brand"
+import Category from "@/models/category"
 import { NextResponse } from "next/server"
 
 export const GET = async () => {
   await connectMongoDB()
-  
+  await Category.find()
   const brands = await Brand.find().populate('parentCategory')
 
   return NextResponse.json({ brands })
--- app/api/products/[id]/route.js
+++ app/api/products/[id]/route.js
@@ -1,4 +1,6 @@
 import connectMongoDB from "@/lib/mongoose"
+import Brand from "@/models/brand"
+import Category from "@/models/category"
 import Product from "@/models/product"
 import { NextResponse } from "next/server"
 
@@ -6,6 +8,8 @@ export async function GET(request, { params }) {
   const { id } = params
 
   await connectMongoDB()
+  await Category.find()
+  await Brand.find().populate('parentCategory')
   const product = await Product.findOne({ _id: id }).populate('selectedCategory').populate('brand')
   
   return NextResponse.json({ product }, { status: 200 })
